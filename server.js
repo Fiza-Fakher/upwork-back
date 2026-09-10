4707186fed799c3de9659011d80e7608f4a09ce0
Fiza Fakher <fizafakher62@gmail.com>
2026-09-10 23:04:12 +0500

    fix the nev issue

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173","https://upwork-back-1ecv.vercel.app"], credentials: true }));
 
 app.use("/api", proposalRoute);
 
@@ -30,3 +30,5 @@ try {
 } catch (error) {
   console.error("Database connection error:", error.message);
 }
+
+export default app;
